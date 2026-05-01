351411766b6a2a8e1190fec2d00cdf6e6ca9b3da
Jakub Kriva <270960@vutbr.cz>
2026-05-01 16:20:32 +0200

    replace

--- LED-PingPong/src/clk_en.vhd
+++ LED-PingPong/src/clk_en.vhd
@@ -1,48 +1,35 @@
-  library ieee;
-use ieee.std_logic_1164.all;
-
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+-------------------------------------
 entity clk_en is
-    port (
-        clk     : in  std_logic;
-        cnt_set : in  std_logic_vector(2 downto 0);
-        rst     : in  std_logic;
-        ce      : out std_logic
-    );
+    generic (G_MAX : positive := 5);
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           ce : out STD_LOGIC);
 end clk_en;
-
+-------------------------------------
 architecture Behavioral of clk_en is
-    signal sig_cnt : integer range 0 to 9 := 0;
-    signal max_cnt : integer range 1 to 9 := 9;
+    signal sig_cnt : integer range 0 to G_MAX-1;
 begin
-
-    process(cnt_set)
+ process (clk) is
     begin
-        case cnt_set is
-            when "000" => max_cnt <= 9;
-            when "001" => max_cnt <= 8;
-            when "010" => max_cnt <= 7;
-            when "011" => max_cnt <= 6;
-            when "100" => max_cnt <= 5;
-            when "101" => max_cnt <= 4;
-            when "110" => max_cnt <= 3;
-            when others => max_cnt <= 2;
-        end case;
-    end process;
+        if rising_edge(clk) then  -- Synchronous process
+            if rst = '1' then     -- High-active reset
+                ce      <= '0';   -- Reset output
+                sig_cnt <= 0;     -- Reset internal counter
 
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if rst = '1' then
-                ce <= '0';
+            elsif sig_cnt = G_MAX-1 then
+                -- TODO: Set output pulse and reset internal counter
+                ce      <= '1';
                 sig_cnt <= 0;
-            elsif sig_cnt = max_cnt then
-                ce <= '1';
-                sig_cnt <= 0;
+
             else
-                ce <= '0';
+                -- TODO: Clear output and increment internal counter
+                ce      <= '0';
                 sig_cnt <= sig_cnt + 1;
-            end if;
-        end if;
+
+            end if;  -- End if for reset/check
+        end if;      -- End if for rising_edge
     end process;
 
 end Behavioral;
